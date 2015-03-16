8037b7fc8cdea49d431dc69128c030a3bfea8d40
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2015-03-16 17:11:39 +0530

    WIP

--- MediumProgressView-Sample/MediumProgressView-Sample/ViewController.swift
+++ MediumProgressView-Sample/MediumProgressView-Sample/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
         case Stop
     }
 
-    //var mediumProgressViewManager: MediumProgressViewManager?
     var status: Motion?
     
     override func viewDidLoad() {
@@ -23,7 +22,7 @@ class ViewController: UIViewController {
         
         let mediumProgressViewManager = MediumProgressViewManager.sharedInstance
         mediumProgressViewManager.position = .Top // Default is top.
-        mediumProgressViewManager.color    = MEDIUM_PROGRESS_COLOR // Default is UIColor(red:0.33, green:0.83, blue:0.44, alpha:1).
+        mediumProgressViewManager.color    = UIColor(red:0.33, green:0.83, blue:0.44, alpha:1) // Default is UIColor(red:0.33, green:0.83, blue:0.44, alpha:1).
         mediumProgressViewManager.height   = 4.0 // Default is 4.0.
         mediumProgressViewManager.isLeft   = true // Default is true.
         mediumProgressViewManager.duration = 1.0  // Default is 1.2.
